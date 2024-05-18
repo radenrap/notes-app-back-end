4cfd238db833f23bc2857a06f03965fa55721fd6
Raul Ananda Putra <raul.anandaputra@gmail.com>
2024-05-18 23:05:38 +0700

    change port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 5050,
+        port: 5000,
         host: 'localhost',
         routes: {
             cors: {
